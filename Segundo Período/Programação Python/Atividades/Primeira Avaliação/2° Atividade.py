bdac1cc2eb836a82a56946edb7b3e34511baf221
Álvaro Lúcio Mousinho Coelho <alvarolucio2007@proton.me>
2025-09-05 21:05:50 -0300

    Update 2° Atividade.py

--- Segundo Período/Programação Python/Atividades/Primeira Avaliação/2° Atividade.py
+++ Segundo Período/Programação Python/Atividades/Primeira Avaliação/2° Atividade.py
@@ -1,28 +1,35 @@
 #Aluno: Álvaro Lúcio Mousinho Coelho 18/08/2025
 def simple_time_conversion(time):
+    if time<0 or type(time)!=int:
+        raise ValueError
     return (time//60,time%60)
 
 def weighted_mean(g1,g2,g3,w1,w2,w3):
     return ((g1*w1)+(g2*w2)+(g3*w3))/(w1+w2+w3)
 
 def variable_switch(var_1,var_2):
-    var_1=var_1-var_2 # original= a,b , target= b,a , var_1=a-b var_2=b -> a-b, b #ex: a=1, b=2 -> var_1=1-2 = -1, var_2=2
-    var_2=var_2+var_1 #var_1=a-b, var_2= a-b+b=a -> a-b, a #ex: var_1=-1, var_2=-1+2 = 1 
-    var_1=-var_1+var_2 #var_1=-(a-b)+a=b , var_2=a ->b,a #ex: var_1=-(1-2)+1= 2, b=1
+    var_1=var_1-var_2 
+    var_2=var_2+var_1 
+    var_1=-var_1+var_2 
     return (var_1,var_2) 
 
 import math as m
 def bhaskara(a,b,c):
-    delta=(b**2 - 4*a*c)
+    delta=(b**2 - 4*a*c)    
     if a!=0 and delta>=0:
-        return ((str(((-b+m.sqrt(delta))/(2*a))),str(((-b-m.sqrt(delta))/(2*a))))) #Real roots
+        return (((-b+m.sqrt(delta))/(2*a))),((-b-m.sqrt(delta))/(2*a)) #Real roots
     elif delta<0:  
-        return ((str(((-b+m.sqrt(-delta))/(2*a))))+"i",str(((-b-m.sqrt(-delta))/(2*a)))+"i") #Imaginary roots
+        real_part=-b/(2*a)
+        complex_part=(m.sqrt(-delta)/(2*a))
+        return (complex(real_part,complex_part),complex(real_part,-complex_part)) #Imaginary roots
     else:
-        raise ZeroDivisionError
+        if b!=0:
+            return -c/b
+        else:
+            raise ValueError("Invalid equation! Doesn't have any variables!")
     
-def unit_conversion(unit):  
-    return (unit/1000,unit*100,unit*1000)
+def unit_conversion(unit):
+    return {"kilo":unit/1000,"centi":unit*100,"mili":unit*1000}
 
 def number_inverted(number): #Original number: abc
     number_unit=number%10 # Will get c
@@ -32,6 +39,8 @@ def number_inverted(number): #Original number: abc
     return inverted
 
 def simple_interest_calculation(P,i,t):
+    i=i/100 if i>1 else i
+    if P<=0 or t<0 or i<0:
+        raise ValueError("All three numbers must be positive!")
     interest= P*i*t #T is monthly
-
-    return f"The final value is {P+interest}"
+    return P+interest
