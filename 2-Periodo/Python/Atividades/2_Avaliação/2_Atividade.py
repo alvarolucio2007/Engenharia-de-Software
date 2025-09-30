edcae21c596b44d6ff19e35cb5a7d4cd29aac167
Álvaro Lúcio <alvarolucio2007@gmail.com>
2025-09-30 07:43:04 -0300

    atualizando tarefa.

--- 2-Periodo/Python/Atividades/2_Avaliação/2_Atividade.py
+++ 2-Periodo/Python/Atividades/2_Avaliação/2_Atividade.py
@@ -1,16 +1,28 @@
-#Aluno: Álvaro Lúcio Mousinho Coelho 22/09/2025
+#Álvaro Lúcio Mousinho Coelho
+#29/09/2025
 
-#Exercício 1:  Criar e Imprimir uma lista de compras
+#Exercício 1:
+def exercise_1(list_supermarket):
+    list_supermarket=["Milk","Cheese","Eggs","Butter","Bread"]
+    list_supermarket.append("Carvão")
+    list_supermarket.append("Picanha")
+    list_supermarket.pop(1)
+    return list_supermarket
 
-shopping_list=[]
-shopping_list.append("Pão")
-shopping_list.append("Leite")
-shopping_list.append("Ovos")
-shopping_list.append("Frutas")
-print(shopping_list)
+#Exercício 2:
+def exercise_2(tuple_ ):
+    tuple_=("Monday","Tuesday","Wednesday","Thursday","Friday")
+    tuple_[0]="Holiday" #Erro, tuplas são imutáveis.
 
-#Exercício 2: Criar um dicionário, com título, ano e diretor
+#Exercício 3:
+def exercise_3(movie):
+    movie_title=input("What's the title of the movie?")
+    movie_year=input("What's the year which the movie has been released?")
+    movie_director=input("What's the director of the movie?")
+    movie={"Title":movie_title, "Year":movie_year,"Director":movie_director}
+    return movie["Year"]
 
-filme={"Título": ["Harry Potter","Titanic"], "Ano": 2001, "Diretor": "Didi"}
-filme["Título"].append("Teste")
-print(filme["Título"])
+#Exercício 4:
+def exercise_4(list_):
+    return set(list_)
+print(exercise_4([1,2,2,3,4,4,5,1]))
