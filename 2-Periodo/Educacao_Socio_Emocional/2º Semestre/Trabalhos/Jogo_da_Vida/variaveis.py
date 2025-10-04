1d5d5468b0f4deb1c6ece9d3a73f39cb75d3fba0
Álvaro Lúcio <alvarolucio2007@gmail.com>
2025-10-04 16:00:53 -0300

    começando a trabalhar no jogo

---
+++ 2-Periodo/Educacao_Socio_Emocional/2º Semestre/Trabalhos/Jogo_da_Vida/variaveis.py
@@ -0,0 +1,6 @@
+import json
+
+caminho_person_img = 'imagens/chaves.png'
+
+with open('Engenharia-de-Software/2-Periodo/Educacao_Socio_Emocional/dados/perg_resp.json') as f:
+    dados = json.load(f) 
--- 2-Periodo/Educacao_Socio_Emocional/main_jogo.py
+++
@@ -1,4 +0,0 @@
-
-import customtkinter as ctk #importar o q faz janelas
-janela=ctk.CTk()
-janela.mainloop()
