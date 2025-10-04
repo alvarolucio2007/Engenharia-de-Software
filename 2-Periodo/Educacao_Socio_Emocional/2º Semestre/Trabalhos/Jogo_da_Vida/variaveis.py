27c47785b037856770a6d50205572dfa6e2c96e2
Álvaro Lúcio <alvarolucio2007@gmail.com>
2025-10-04 19:35:43 -0300

    ;

--- 2-Periodo/Educacao_Socio_Emocional/2º Semestre/Trabalhos/Jogo_da_Vida/variaveis.py
+++
@@ -1,6 +0,0 @@
-import json
-
-caminho_person_img = 'imagens/chaves.png'
-
-with open('Engenharia-de-Software/2-Periodo/Educacao_Socio_Emocional/dados/perg_resp.json') as f:
-    dados = json.load(f) 
