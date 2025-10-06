e67db1725a88f32b1d6d245c905573437f5e41d1
Álvaro Lúcio <alvarolucio2007@gmail.com>
2025-10-06 12:01:28 -0300

    .

--- 2-Periodo/Python/Atividades/2_Avaliação/2_Atividade.py
+++ 2-Periodo/Python/Atividades/2_Avaliação/2_Atividade.py
@@ -132,5 +132,4 @@ def exercicio_14(list_): #TODO: fazer funcionar
                     print("Please insert only 'y' or 'n'!")
                 if user_book_answer=="n":
                     break
-
 print(exercicio_14([{"Nome": "João", "Quantidade": 3},{"Nome": "Maria", "Quantidade":1}]))   
