be13d961200639c593209ee53c96251e1b263cbf
Álvaro Lúcio <alvarolucio2007@proton.me>
2025-10-27 15:52:23 -0300

    .

--- 2-Periodo/Python/Atividades/3_Avaliação/1_Atividade.py
+++ 2-Periodo/Python/Atividades/3_Avaliação/1_Atividade.py
@@ -3,10 +3,11 @@ def exercicio_1(nome,saudacao):
     return f"{saudacao}, {nome}!" if type(nome)==str and type(saudacao)==str and len(nome)>0 and len(saudacao)>0 else "Erro! Por favor, tente novamente com nomes válidos!"
 
 #Segundo exercício: Calculadora para somar, subtrair, multiplicar e dividir 2 números.
-def exercicio_2(num_1,num_2):
-    return (num_1+num_2,num_1-num_2,num_1*num_2,num_1/num_2) if num_2!=0 else "Erro! Divisão por 0!"
-print(exercicio_2(1,0))
-
+def exercicio_2(num_1,num_2, op): #TODO: fazer mais personalizável ao usuário
+    if op in ["+","-","*","/","^"]:
+        return eval(str(num_1)+str(op).replace("^","**")+str(num_2)) if num_2!=0 else "Erro! Divisão por 0!"
+    else:
+        raise ValueError
 #Terceiro exercício: Validar se uma nota está entre 0 e 10, retornando T/F
 def exercicio_3(num):
     return True if 0<= num <=10  else False
