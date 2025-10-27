3142c705ee7169e3782ceb36090e606c18fb4f5a
Álvaro Lúcio <alvarolucio2007@proton.me>
2025-10-27 12:19:40 -0300

    tarefas

--- 2-Periodo/Python/Atividades/3_Avaliação/1_Atividade.py
+++ 2-Periodo/Python/Atividades/3_Avaliação/1_Atividade.py
@@ -1,6 +1,6 @@
 #Primeiro exercício: Função para recerber um nome e retornar uma saudação personalizada.
 def exercicio_1(nome,saudacao):
-    return f"{saudacao},{nome}!"
+    return f"{saudacao}, {nome}!" if type(nome)==str and type(saudacao)==str and len(nome)>0 and len(saudacao)>0 else "Erro! Por favor, tente novamente com nomes válidos!"
 
 #Segundo exercício: Calculadora para somar, subtrair, multiplicar e dividir 2 números.
 def exercicio_2(num_1,num_2):
@@ -9,5 +9,5 @@ print(exercicio_2(1,0))
 
 #Terceiro exercício: Validar se uma nota está entre 0 e 10, retornando T/F
 def exercicio_3(num):
-    return True if num in range(0,11) else False
+    return True if 0<= num <=10  else False
 
