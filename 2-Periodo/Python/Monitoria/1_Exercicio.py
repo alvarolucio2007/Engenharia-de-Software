4bcea2713d033d422f713ec4d00f7ee280fde178
Álvaro Lúcio <alvarolucio2007@proton.me>
2025-11-05 09:42:31 -0300

    otimizando

--- 2-Periodo/Python/Monitoria/1_Exercicio.py
+++ 2-Periodo/Python/Monitoria/1_Exercicio.py
@@ -1,20 +1,20 @@
 #Questão 1: Calculadora de IMC
 def calcular_imc(peso,altura):
     imc=peso/altura**2
-    if imc<=17:
-        return f"Muito abaixo do peso! IMC= {imc}"
+    if imc<=18.5:
+        return f"Muito abaixo do peso! IMC= {imc:.2f}"
     elif imc<=18.49:
-        return f"Abaixo do peso! IMC={imc}"
+        return f"Abaixo do peso! IMC={imc:.2f}"
     elif imc<=24.99:
-        return f"Peso normal! IMC={imc}"
+        return f"Peso normal! IMC={imc:.2f}"
     elif imc<=29.99:
-        return f"Acima do peso! IMC={imc}"
+        return f"Acima do peso! IMC={imc:.2f}"
     elif imc<=34.99:
-        return f"Obesidade I! IMC={imc}"
+        return f"Obesidade I! IMC={imc:.2f}"
     elif imc <=39.99:
-        return f"Obesidade II (severa)! IMC={imc}"
+        return f"Obesidade II (severa)! IMC={imc:.2f}"
     else:
-        return f"Obesidade III (mórbida)! IMC={imc}"
+        return f"Obesidade III (mórbida)! IMC={imc:.2f}"
 
 #Conversor de temperatura bem simples de C pra F
 def converter_temperatura(celsius):
@@ -23,5 +23,10 @@ print(converter_temperatura(25))
 
 #Verificação de números primos pythonico.
 def verificar_primo(numero):
-    return not any(x for x in range(2,numero) if numero%x==0)
-print(verificar_primo(2))
+    if numero<0:
+        return  "Números negativos não podem ser aceitos!"
+    elif numero==0 or numero==1:
+        return False
+    import math
+    return not any(numero % x == 0  for x in range(3,int(math.sqrt(numero)+1),2))
+print(verificar_primo(1000000))
