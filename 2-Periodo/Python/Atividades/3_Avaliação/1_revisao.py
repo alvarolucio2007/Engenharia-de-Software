b9488e05f40f494e29ea6eb38e7aaf7d06608480
Álvaro Lúcio <alvarolucio2007@proton.me>
2025-11-11 20:41:43 -0300

    Terminando 3 tarefa. ok

--- 2-Periodo/Python/Atividades/3_Avaliação/1_revisao.py
+++ 2-Periodo/Python/Atividades/3_Avaliação/1_revisao.py
@@ -1,19 +1,17 @@
+#Questão 1
 def saudar() -> None:
     print("Bem-vindo ao módulo de funções em Python!")
-    return
+#Questão 2
 def dobrar_numero(numero:int) -> int: 
-    dobro=int(2*numero)
-    return dobro
+    return 2*numero
+#Questão 3
 def eh_inteiro(numero:int) -> bool:
     return isinstance(numero,int)
+#Questão 4
+def calcular_preco_final(preco:float,desconto:float = 0.1) -> float:
+    return preco*(1-desconto)
+#Questão 5: Dentro da função, x será 10, mas fora, será 50.
 
-def calcular_preco_final(preco_original:float,*desconto:float) -> float|bool:
-    if desconto and (desconto[0])<=100:
-        desconto_produto_args:float=(preco_original*desconto[0]/100)
-        return preco_original-desconto_produto_args
-    elif desconto and len(desconto)!=1:
-        return False
-    else:
-        desconto_produto:float=(preco_original*0.1)
-        return preco_original-desconto_produto
-print(calcular_preco_final(100,75)) 
+#Questão 6
+def contar_pares(lista:list) -> int :
+    return len([d for d in lista if d%2==0])
