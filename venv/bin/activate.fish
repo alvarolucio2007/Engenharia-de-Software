ffce12e747181664eae8ff64c5ced4ee9b33ced4
Álvaro Lúcio Mousinho Coelho <alvarolucio2007@proton.me>
2025-11-24 08:59:28 -0300

    .

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/alvaro/Códigos/Engenharia-de-Software/venv'
+set -gx VIRTUAL_ENV '/home/user/Códigos/Engenharia-de-Software/venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
