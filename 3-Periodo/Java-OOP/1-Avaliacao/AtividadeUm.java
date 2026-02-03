dac3bdc6d8b7524025c2ff0edb828ea39092cad3
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-02-03 07:58:01 -0300

    Atualizando Java

--- 3-Periodo/Java-OOP/1-Avaliacao/AtividadeUm.java
+++ 3-Periodo/Java-OOP/1-Avaliacao/AtividadeUm.java
@@ -3,13 +3,3 @@ public class AtividadeUm {
     System.out.println("Hello World!");
   }
 }
-
-class Livro {
-  private String titulo;
-  private double preco;
-
-  public Livro(String titulo, double preco) {
-    this.titulo = titulo;
-    this.preco = preco;
-  }
-}
