b57c386d3f6ca2e60ab3e7c6cf1bf5c04ecc4e81
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-02-03 07:52:24 -0300

    Adicionando Java

---
+++ 3-Periodo/Java-OOP/1-Avaliacao/AtividadeUm.java
@@ -0,0 +1,15 @@
+public class AtividadeUm {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
+
+class Livro {
+  private String titulo;
+  private double preco;
+
+  public Livro(String titulo, double preco) {
+    this.titulo = titulo;
+    this.preco = preco;
+  }
+}
