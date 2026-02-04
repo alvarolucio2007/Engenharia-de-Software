96c2ba50182ad9bbf4762b11be2e05144956b187
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-02-03 21:38:22 -0300

    Começando a OOP em Java

--- 3-Periodo/Java-OOP/1-Avaliacao/AtividadeUm.java
+++ 3-Periodo/Java-OOP/1-Avaliacao/AtividadeUm.java
@@ -1,5 +1,67 @@
+class Livro {
+  String titulo;
+  String autor;
+  float preco;
+  protected int ano;
+
+  public int getAno() {
+    return ano;
+  }
+
+  public void setAno(int novoAno) {
+    if (novoAno > 0) {
+      this.ano = novoAno;
+    } else {
+      System.out.println("Ano AC!");
+    }
+  }
+
+  public void setPreco(float novoPreco) {
+    if (novoPreco > 0) {
+      this.preco = novoPreco;
+
+    } else {
+      System.out.println("Preço Inválido!");
+    }
+  }
+
+  void ler() {
+    System.out.println(
+        "O livro " + titulo + " do autor " + autor + " foi lançado em " + ano + " com o preço de " + preco + ".");
+  }
+}
+
+class Ebook extends Livro {
+  protected double tamanhoArquivo;
+
+  public double getTamanhoArquivo() {
+    return tamanhoArquivo;
+  }
+
+  public void setTamanhoArquivo(double novoTamanhoArquivo) {
+    if (novoTamanhoArquivo > 0) {
+      this.tamanhoArquivo = novoTamanhoArquivo;
+    } else {
+      System.out.println("Tamanho inválido!");
+    }
+  }
+
+  @Override
+  void ler() {
+    System.out.println(
+        "O livro " + titulo + " do autor " + autor + " foi lançado em " + ano + " com o preço de " + preco + "e contém "
+            + tamanhoArquivo + "mB de espaço.");
+  }
+}
+
 public class AtividadeUm {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    Ebook meuEbook = new Ebook();
+    meuEbook.titulo = "Teste";
+    meuEbook.autor = "Teste";
+    meuEbook.setPreco(100.0f);
+    meuEbook.setAno(100);
+    meuEbook.setTamanhoArquivo(1000d);
+    meuEbook.ler();
   }
 }
