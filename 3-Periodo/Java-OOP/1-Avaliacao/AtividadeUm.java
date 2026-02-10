aa7a6a882a6a10cfc719692ba5ee70f32d671cec
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-02-10 13:48:22 -0300

    Colocando atividade.

--- 3-Periodo/Java-OOP/1-Avaliacao/AtividadeUm.java
+++ 3-Periodo/Java-OOP/1-Avaliacao/AtividadeUm.java
@@ -1,67 +1,46 @@
-class Livro {
-  String titulo;
-  String autor;
-  float preco;
-  protected int ano;
-
-  public int getAno() {
-    return ano;
-  }
-
-  public void setAno(int novoAno) {
-    if (novoAno > 0) {
-      this.ano = novoAno;
-    } else {
-      System.out.println("Ano AC!");
-    }
-  }
-
-  public void setPreco(float novoPreco) {
-    if (novoPreco > 0) {
-      this.preco = novoPreco;
-
-    } else {
-      System.out.println("Preço Inválido!");
-    }
-  }
-
-  void ler() {
-    System.out.println(
-        "O livro " + titulo + " do autor " + autor + " foi lançado em " + ano + " com o preço de " + preco + ".");
-  }
-}
-
-class Ebook extends Livro {
-  protected double tamanhoArquivo;
-
-  public double getTamanhoArquivo() {
-    return tamanhoArquivo;
-  }
-
-  public void setTamanhoArquivo(double novoTamanhoArquivo) {
-    if (novoTamanhoArquivo > 0) {
-      this.tamanhoArquivo = novoTamanhoArquivo;
-    } else {
-      System.out.println("Tamanho inválido!");
-    }
-  }
-
-  @Override
-  void ler() {
-    System.out.println(
-        "O livro " + titulo + " do autor " + autor + " foi lançado em " + ano + " com o preço de " + preco + "e contém "
-            + tamanhoArquivo + "mB de espaço.");
-  }
-}
+import java.util.Scanner;
 
 public class AtividadeUm {
   public static void main(String[] args) {
-    Ebook meuEbook = new Ebook();
-    meuEbook.titulo = "Teste";
-    meuEbook.autor = "Teste";
-    meuEbook.setPreco(100.0f);
-    meuEbook.setAno(100);
-    meuEbook.setTamanhoArquivo(1000d);
-    meuEbook.ler();
+    Scanner scanner = new Scanner(System.in);
+    double saldo = 1000d;
+    System.out.println("1: Consultar Saldo, 2: Depositar Valor, 3: Sacar Valor, 4: Sair");
+    int opcao;
+    do {
+      opcao = scanner.nextInt();
+      switch (opcao) {
+        case 1:
+          System.out.println(saldo);
+          break;
+        case 2:
+          System.out.println("Por favor, insira o valor que voce quer adicionar!");
+          double adicionar = scanner.nextDouble();
+          if (adicionar < 0) {
+            System.out.println("Você não pode depoistar valores menores que 0!");
+          } else {
+            saldo += adicionar;
+          }
+          System.out.println("Você agora possui " + saldo);
+          break;
+        case 3:
+          System.out.println("Por favor, insira o valor que quiser retirar! (Tem que ser menor que o saldo.)");
+          double remover;
+          remover = scanner.nextDouble();
+          if (remover <= saldo) {
+            saldo -= remover;
+          } else {
+            System.out.println("Saldo insuficiente!");
+          }
+          System.out.println("Vocẽ agora possui " + saldo);
+          break;
+        case 4:
+          System.out.println("Obrigado por ter saido do sistema, saindo...");
+          break;
+        default:
+          System.out.println("Por favor, insira apenas de 1 a 4");
+          break;
+      }
+    } while (opcao != 4);
+    scanner.close();
   }
 }
