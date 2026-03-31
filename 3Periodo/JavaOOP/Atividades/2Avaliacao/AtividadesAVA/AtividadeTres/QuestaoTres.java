7429cfce26f321db365c91ea47922d7c6576bf2e
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-03-31 18:01:26 -0300

    Implementando algumas classes do exercício.

--- 3Periodo/JavaOOP/Atividades/2Avaliacao/AtividadesAVA/AtividadeTres/QuestaoTres.java
+++ 3Periodo/JavaOOP/Atividades/2Avaliacao/AtividadesAVA/AtividadeTres/QuestaoTres.java
@@ -1,3 +1,48 @@
 class Ingresso {
   protected double reais;
+
+  public double getReais() {
+    return reais;
+  }
+
+  public void setReais(double reais) {
+    this.reais = reais;
+  }
+
+  public Ingresso(double reais) {
+    this.reais = reais;
+  }
+
+  protected void imprimeValor() {
+    System.out.println(this.reais);
+  }
+}
+
+class VIP extends Ingresso {
+  protected double adicionalVIP;
+
+  public double getAdicionalVIP() {
+    return adicionalVIP;
+  }
+
+  public void setAdicionalVIP(double adicionalVIP) {
+    this.adicionalVIP = adicionalVIP;
+  }
+
+  public VIP(double reais, double adicionalVIP) {
+    super(reais);
+    this.adicionalVIP = adicionalVIP;
+  }
+
+}
+
+class Normal extends Ingresso {
+
+  public Normal(double reais) {
+    super(reais);
+  }
+
+  private void impromeIngresso() {
+    System.out.println("Ingresso Normal");
+  }
 }
