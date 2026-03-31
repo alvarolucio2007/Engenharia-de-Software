1b398d47e46978b92ac8ead7d503b966dc60f2f5
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-03-31 18:43:42 -0300

    Fazendo o desafio de herança.

---
+++ 3Periodo/JavaOOP/Atividades/2Avaliacao/AtividadesAVA/DesafioHeranca.java
@@ -0,0 +1,6 @@
+class Veiculo {
+  protected String especie, placa, marca, modelo, chassi, renavam, procedencia, corExterna, corInterna, tipoCombustivel,
+      motor, opcionais;
+  protected int anoModelo, anoFabricacao, numeroPortas, numeroPassageiros;
+  protected double capacidadeMaximaCarga, altura, largura, profundidade, consumoMedio;
+}
