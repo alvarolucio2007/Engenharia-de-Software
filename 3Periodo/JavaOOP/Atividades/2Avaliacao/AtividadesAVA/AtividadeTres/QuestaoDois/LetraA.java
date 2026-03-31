d467de2e565b14732c92b55d3c75f1e809e6b8ae
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-03-31 11:20:23 -0300

    Terminando a letra A.

---
+++ 3Periodo/JavaOOP/Atividades/2Avaliacao/AtividadesAVA/AtividadeTres/QuestaoDois/LetraA.java
@@ -0,0 +1,53 @@
+abstract class Animal {
+  protected String nome;
+  protected String raca;
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getRaca() {
+    return raca;
+  }
+
+  public void setRaca(String raca) {
+    this.raca = raca;
+  }
+
+  public Animal(String nome, String raca) {
+    this.nome = nome;
+    this.raca = raca;
+  }
+
+  protected String caminha() {
+    return "Caminhando...";
+  }
+
+}
+
+class Cachorro extends Animal {
+
+  public Cachorro(String nome, String raca) {
+    super(nome, raca);
+  }
+
+  private String latir() {
+    return "Latindo...";
+
+  }
+}
+
+class Gato extends Animal {
+
+  public Gato(String nome, String raca) {
+    super(nome, raca);
+  }
+
+  private String miar() {
+    return "Miando...";
+  }
+}
