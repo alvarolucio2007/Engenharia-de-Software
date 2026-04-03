ed5225b37a097f561f92bbde2ab2fd310bac878a
Álvaro Lúcio Mousinho Coelho <alvarolucio2007@proton.me>
2026-04-03 19:54:10 -0300

    Add FULL JOIN example SQL script
    Create FULL JOIN example with two student tables.

---
+++ 3Periodo/bancosDeDados/atividadeJoins/FULLJOINExemplo.sql
@@ -0,0 +1,22 @@
+
+CREATE TABLE IF NOT EXISTS alunos(
+  first_name TEXT,
+  AGE INTEGER
+);
+INSERT INTO alunos VALUES
+  ('João',25),
+  ('Maria',30);
+
+CREATE TABLE IF NOT EXISTS alunos2(
+  first_name TEXT,
+  AGE INTEGER
+);
+INSERT INTO alunos2 VALUES
+  ('Fulano',15),
+  ('Sicrano',20);
+  
+  SELECT * FROM alunos;
+  SELECT * FROM alunos2;
+  
+SELECT * FROM alunos FULL JOIN alunos2 ON alunos.AGE=alunos2.AGE
+
