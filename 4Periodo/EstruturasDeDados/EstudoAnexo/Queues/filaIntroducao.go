547176c895a958982b72639a57b56d881d99df1b
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-07-13 21:08:35 -0300

    feat: Adicionando filas

---
+++ 4Periodo/EstruturasDeDados/EstudoAnexo/Queues/filaIntroducao.go
@@ -0,0 +1,5 @@
+package main
+
+// O que é uma fila/queue: Estrutura de dados LILO (Last in Last out)/FIFO (First in First out)
+// Inserção de dados: mais recente; Remoção: Mais antigo.
+// Exemplos de uso: Fila normal kkkkkkkkkkkkkkkkkkkkk
