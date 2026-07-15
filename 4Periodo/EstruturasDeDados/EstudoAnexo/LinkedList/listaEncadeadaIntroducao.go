3d125b32fa6f47ad3b9a9834983715edae6cbe79
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-07-14 21:50:57 -0300

    feat: Começando estudos de Listas Encadeadas.

---
+++ 4Periodo/EstruturasDeDados/EstudoAnexo/LinkedList/listaEncadeadaIntroducao.go
@@ -0,0 +1,12 @@
+package main
+
+// Listas lineares sequenciais são vetores
+// Listas encadeadas não são vetores
+// Listas encadeadas são listas lineares dispersas, mas a ordem lógica entre os elementos é mantido.
+
+// Vantagens lista linear: Fácil acesso a cada elemento, O(1)
+// Desvantagens lista linear: Precisa alocar espaço suficiente pra todos os elementos de uma vez, memória alocada não utilizada
+// Também adicionar ou remover elementos pode ser difícil.
+//
+// Vantagens lista encadeada: Fácil inserção e remoção de elementos O(1), número de elementos variável
+//
