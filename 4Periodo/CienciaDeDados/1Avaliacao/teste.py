ffca4bcd3f36af34b883c36812fc62d1a63b23ab
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-08-12 12:03:49 -0300

    feat: Adicionando descrição

--- 4Periodo/CienciaDeDados/1Avaliacao/teste.py
+++ 4Periodo/CienciaDeDados/1Avaliacao/teste.py
@@ -1,3 +1,4 @@
+#OBJETIVOS: Carregar dataset, Inspecionar head info e describe, verificar nulos e corrigir, criar 2 graficos e salvar o arquivo modificado.
 import pandas as pd
 FILE_PATH="aula3_base_problema.csv"
 df_csv=pd.read_csv(FILE_PATH)
