934934e4a74c00a1e15ec5a541a84de6d11f308d
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-08-12 18:16:28 -0300

    feat: Começando novamente a atividade.

--- 4Periodo/CienciaDeDados/1Avaliacao/teste.py
+++ 4Periodo/CienciaDeDados/1Avaliacao/teste.py
@@ -1,6 +1,10 @@
 #OBJETIVOS: Carregar dataset, Inspecionar head info e describe, verificar nulos e corrigir, criar 2 graficos e salvar o arquivo modificado.
 import pandas as pd
 FILE_PATH="aula3_base_problema.csv"
-df_csv=pd.read_csv(FILE_PATH)
-print(df_csv)
-print("Hello world")
+df=pd.read_csv(FILE_PATH)
+def separador(){
+    print(60*"=")
+}
+separador()
+print("DATASET ORIGINAL:")
+
