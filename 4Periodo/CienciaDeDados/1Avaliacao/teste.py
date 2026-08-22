cede0d0fe77cd2fd8e45a9038007f5e043e10d6f
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-08-22 00:55:01 -0300

    fix: Ajustando imports.

--- 4Periodo/CienciaDeDados/1Avaliacao/teste.py
+++ 4Periodo/CienciaDeDados/1Avaliacao/teste.py
@@ -1,8 +1,6 @@
 # OBJETIVOS: Carregar dataset, Inspecionar head info e describe, verificar nulos e corrigir, criar 2 graficos e salvar o arquivo modificado.
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+import pandas as pd
 
 FILE_PATH = "aula3_base_problema.csv"
 df = pd.read_csv(FILE_PATH)
