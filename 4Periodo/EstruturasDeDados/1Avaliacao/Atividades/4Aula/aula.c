e700a8ef94169a0aa5795afe03a02f7ab95742e5
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-08-24 08:57:23 -0300

    fix: Ajustando scanf e printf

--- 4Periodo/EstruturasDeDados/1Avaliacao/Atividades/4Aula/aula.c
+++ 4Periodo/EstruturasDeDados/1Avaliacao/Atividades/4Aula/aula.c
@@ -3,7 +3,8 @@ int main() {
   int vetor_int[5];
   for (int i = 0; i < 5; i++) {
     int numero_add;
-    scanf("Qual o inteiro que quer adicionar? %i\n", &numero_add);
+    printf("Qual o inteiro que quer adicionar?\n");
+    scanf("%i", &numero_add);
     vetor_int[i] = numero_add;
   }
   int soma = 0;
