36b42db1fa3f2571e55204f333d4a5fd8ed82d08
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-09-16 10:19:30 -0300

    feat: Adicionando main.rs teste

--- 4Periodo/EstruturasDeDados/1Avaliacao/testeEstruturasDeDados/src/main.rs
+++ 4Periodo/EstruturasDeDados/1Avaliacao/testeEstruturasDeDados/src/main.rs
@@ -1,3 +1,11 @@
+mod testePilha;
+
+use std::io;
 fn main() {
     println!("Hello, world!");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read line");
+    println!("You guessed: {guess}");
 }
